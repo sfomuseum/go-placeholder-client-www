ace8060676c8c8e877629d6fbdb68828830779c4
thisisaaronland <devnull@localhost>
2020-01-29 11:06:14 -0800

    start moving filters code in to http/filters.go

---
+++ http/filters.go
@@ -0,0 +1,60 @@
+package http
+
+import (
+	"github.com/sfomuseum/go-placeholder-client/filters"
+	wof_sanitize "github.com/whosonfirst/go-sanitize"
+	gohttp "net/http"
+	"strings"
+)
+
+func SearchFiltersWithRequest(req *gohttp.Request) ([]filters.Filter, error) {
+
+	var search_filters []filters.Filter
+
+	possible_filters := []string{
+		"lang",
+		"placetype",
+		"mode",
+	}
+
+	q := req.URL.Query()
+
+	sn_opts := wof_sanitize.DefaultOptions()
+
+	for _, k := range possible_filters {
+
+		v, ok := q[k]
+
+		if !ok {
+			continue
+		}
+
+		if len(v) == 0 {
+			continue
+		}
+
+		sanitized_k, err := wof_sanitize.SanitizeString(k, sn_opts)
+
+		if err != nil {
+			return nil, err
+		}
+
+		str_v := strings.Join(v, ",")
+
+		sanitized_v, err := wof_sanitize.SanitizeString(str_v, sn_opts)
+
+		if err != nil {
+			return nil, err
+		}
+
+		f, err := filters.NewSearchFilter(sanitized_k, sanitized_v)
+
+		if err != nil {
+			return nil, err
+		}
+
+		search_filters = append(search_filters, f)
+	}
+
+	return search_filters, nil
+}
--- http/search.go
+++ http/search.go
@@ -4,13 +4,10 @@ import (
 	"errors"
 	"github.com/aaronland/go-http-sanitize"
 	"github.com/sfomuseum/go-placeholder-client"
-	"github.com/sfomuseum/go-placeholder-client/filters"
 	"github.com/sfomuseum/go-placeholder-client/results"
-	wof_sanitize "github.com/whosonfirst/go-sanitize"
 	"html/template"
 	_ "log"
 	gohttp "net/http"
-	"strings"
 )
 
 type SearchVars struct {
@@ -40,8 +37,6 @@ func NewSearchHandler(opts *SearchHandlerOptions) (gohttp.Handler, error) {
 		"Ancestors": Ancestors,
 	})
 
-	sn_opts := wof_sanitize.DefaultOptions()
-
 	fn := func(rsp gohttp.ResponseWriter, req *gohttp.Request) {
 
 		text, err := sanitize.GetString(req, "text")
@@ -51,52 +46,11 @@ func NewSearchHandler(opts *SearchHandlerOptions) (gohttp.Handler, error) {
 			return
 		}
 
-		var search_filters []filters.Filter
+		search_filters, err := SearchFiltersWithRequest(req)
 
-		possible_filters := []string{
-			"lang",
-			"placetype",
-			"mode",
-		}
-
-		q := req.URL.Query()
-
-		for _, k := range possible_filters {
-
-			v, ok := q[k]
-
-			if !ok {
-				continue
-			}
-
-			if len(v) == 0 {
-				continue
-			}
-
-			sanitized_k, err := wof_sanitize.SanitizeString(k, sn_opts)
-
-			if err != nil {
-				gohttp.Error(rsp, err.Error(), gohttp.StatusBadRequest)
-				return
-			}
-
-			str_v := strings.Join(v, ",")
-
-			sanitized_v, err := wof_sanitize.SanitizeString(str_v, sn_opts)
-
-			if err != nil {
-				gohttp.Error(rsp, err.Error(), gohttp.StatusBadRequest)
-				return
-			}
-
-			f, err := filters.NewSearchFilter(sanitized_k, sanitized_v)
-
-			if err != nil {
-				gohttp.Error(rsp, err.Error(), gohttp.StatusBadRequest)
-				return
-			}
-
-			search_filters = append(search_filters, f)
+		if err != nil {
+			gohttp.Error(rsp, err.Error(), gohttp.StatusBadRequest)
+			return
 		}
 
 		var search_vars SearchVars
